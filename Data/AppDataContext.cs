9a0d5ac8db801c0f1a00f5519d0e2832fcd149c4
Vinícius Alves Goveia Ramos <vinicius.ramos@4secglobal.com>
2025-01-11 11:41:42 -0300

    Conclusão do curso de Fundamentos ASP.NET Core

--- Data/AppDataContext.cs
+++ Data/AppDataContext.cs
@@ -8,15 +8,20 @@ namespace Blog.Data
 {
     public class AppDataContext : DbContext
     {
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+        {
+            
+        }
+
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<User> Users { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            options.UseSqlServer("Server=localhost,1433;Database=Blog;User=sa;Password=!25GoveiA789;TrustServerCertificate=True");
-            options.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //{
+        //    options.UseSqlServer("Server=localhost,1433;Database=Blog;User=sa;Password=!25GoveiA789;TrustServerCertificate=True");
+        //    options.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
